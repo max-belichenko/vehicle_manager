96a6cfef4ce439004f04e15874a7c7e65b7b6aac
Max Belichenko <maxim.b@carely.group>
2022-07-15 23:46:48 +0300

    Add Views

--- application/api/views.py
+++ application/api/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from django.http import Http404
+from rest_framework import viewsets, status
 from rest_framework import permissions
-from api.serializers import UserSerializer, GroupSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.models import Vehicle, DataLog
+from api.serializers import UserSerializer, GroupSerializer, VehicleSerializer, DataLogSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +25,55 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class VehicleList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        vehicles = Vehicle.objects.all()
+        serializer = VehicleSerializer(vehicles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = VehicleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            response = Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return response
+
+
+class VehicleDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Vehicle.objects.get(pk=pk)
+        except Vehicle.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        vehicle = self.get_object(pk)
+        serializer = VehicleSerializer(vehicle)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        vehicle = self.get_object(pk)
+        serializer = VehicleSerializer(vehicle, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data)
+        else:
+            response = Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return response
+
+    def delete(self, request, pk, format=None):
+        vehicle = self.get_object(pk)
+        vehicle.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

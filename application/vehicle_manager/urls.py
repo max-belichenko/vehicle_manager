fffff3732acb60bec39ea427bd40d28d555e661d
Max Belichenko <maxim.b@carely.group>
2022-07-15 23:49:02 +0300

    Update URLs

--- application/vehicle_manager/urls.py
+++ application/vehicle_manager/urls.py
@@ -1,21 +1,7 @@
-"""vehicle_manager URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from api import views
 
@@ -27,5 +13,9 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('vehicles/', views.VehicleList.as_view()),
+    path('vehicles/<int:pk>/', views.VehicleDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)

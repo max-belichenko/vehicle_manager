20cfacb1ca78c292d378406a78e38a3948885c22
Max Belichenko <maxim.b@carely.group>
2022-07-16 14:53:21 +0300

    Refactor serializers

--- application/api/serializers.py
+++ application/api/serializers.py
@@ -1,67 +1,31 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-from api.exceptions import VehicleAPIException
 from api.models import Vehicle, DataLog
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """ Сериализатор модели User. """
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    """ Сериализатор модели Group. """
     class Meta:
         model = Group
         fields = ['url', 'name']
 
 
 class VehicleSerializer(serializers.ModelSerializer):
-    # Переопределить сериализацию полей
+    """
+    Сериализатор модели DataLog.
+
+    Поля created_by и updated_by переопределены и отображают `User.username` вместо `User.id`.
+    """
     created_by = serializers.SlugRelatedField(read_only=True, slug_field='username', )
     updated_by = serializers.SlugRelatedField(read_only=True, slug_field='username', )
-
-    # def create(self, validated_data):
-    #     """
-    #     Создаёт и возвращает новый объект Vehicle.
-    #     """
-    #     try:
-    #         user = self.context['request'].user
-    #     except KeyError:
-    #         raise VehicleAPIException(
-    #             'VehicleSerializer: Необходимо передать Request, как дополнительный контекст. Пример: '
-    #             'VehicleSerializer(validated_data, context={\'request\': request})'
-    #         )
-    #
-    #     validated_data['created_by'] = user
-    #     validated_data['updated_by'] = user
-    #
-    #     return Vehicle.objects.create(**validated_data)
-
-    # def update(self, instance, validated_data):
-    #     """
-    #     Обновляет и возвращает существующий объект Vehicle.
-    #     """
-    #     try:
-    #         user = self.context['request'].user
-    #     except KeyError:
-    #         raise VehicleAPIException(
-    #             'VehicleSerializer: Необходимо передать Request, как дополнительный контекст. Пример: '
-    #             'VehicleSerializer(validated_data, context={\'request\': request})'
-    #         )
-    #
-    #     instance.updated_by = user
-    #     instance.make = validated_data.get('make', instance.make)
-    #     instance.model = validated_data.get('model', instance.model)
-    #     instance.color = validated_data.get('color', instance.color)
-    #     instance.registration_number = validated_data.get('registration_number', instance.registration_number)
-    #     instance.year_of_manufacture = validated_data.get('year_of_manufacture', instance.year_of_manufacture)
-    #     instance.vin = validated_data.get('vin', instance.vin)
-    #     instance.vehicle_certificate_number = validated_data.get('vehicle_certificate_number', instance.vehicle_certificate_number)
-    #     instance.vehicle_certificate_date = validated_data.get('vehicle_certificate_date', instance.vehicle_certificate_date)
-    #     instance.save()
-    #     return instance
 
     class Meta:
         model = Vehicle
@@ -71,28 +35,20 @@ class VehicleSerializer(serializers.ModelSerializer):
 
 
 class DataLogSerializer(serializers.ModelSerializer):
-    # Переопределить сериализацию полей
-    created_by = serializers.SlugRelatedField(read_only=True, slug_field='username', )
+    """
+    Сериализатор модели DataLog.
 
-    # def create(self, validated_data):
-    #     """
-    #     Создаёт и возвращает новый объект Vehicle.
-    #     """
-    #     try:
-    #         user = self.context['request'].user
-    #     except KeyError:
-    #         raise VehicleAPIException(
-    #             'VehicleSerializer: Необходимо передать Request, как дополнительный контекст. Пример: '
-    #             'VehicleSerializer(validated_data, context={\'request\': request})'
-    #         )
-    #
-    #     validated_data['created_by'] = user
-    #
-    #     return DataLog.objects.create(**validated_data)
+    Поле created_by переопределено и отображает `User.username` вместо `User.id`.
+    """
+    created_by = serializers.SlugRelatedField(read_only=True, slug_field='username', )
 
     class Meta:
         model = DataLog
-        fields = ['id', 'created_at', 'created_by', 'vehicle_id', 'registration_number', 'vin',
-                  'vehicle_certificate_number', 'operation', 'description', ]
-        read_only_fields = ['id', 'created_at', 'created_by', 'vehicle_id', 'registration_number', 'vin',
-                            'vehicle_certificate_number', 'operation', 'description', ]
+        fields = [
+            'id', 'created_at', 'created_by', 'vehicle_id', 'registration_number', 'vin', 'vehicle_certificate_number',
+            'operation', 'description',
+        ]
+        read_only_fields = [
+            'id', 'created_at', 'created_by', 'vehicle_id', 'registration_number', 'vin', 'vehicle_certificate_number',
+            'operation', 'description',
+        ]

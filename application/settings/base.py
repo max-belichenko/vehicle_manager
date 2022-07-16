c8f356af605440ee72438b8ccdcf752f35bb99a1
Max Belichenko <maxim.b@carely.group>
2022-07-16 14:17:12 +0300

    Update app settings

--- application/settings/base.py
+++ application/settings/base.py
@@ -1,11 +1,19 @@
+from enum import Enum
 from pydantic import BaseSettings, Field
 from typing import Optional
+
+
+class EnvironmentEnum(str, Enum):
+    development = 'development'
+    production = 'production'
 
 
 class ApplicationSettings(BaseSettings):
     application_name: Optional[str] = 'Vehicle Manager'
     debug: Optional[bool] = False
     secret_key: str
+    allowed_hosts: list[str]
+    environment: Optional[EnvironmentEnum] = 'development'
 
     class Config:
         env_file = '.env'

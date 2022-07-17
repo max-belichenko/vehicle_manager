6b9b4a8ffa71461da3b38f88f8a2dbc10f80da71
Max Belichenko <maxim.b@carely.group>
2022-07-17 19:49:14 +0300

    Fix .sh

--- create_superuser.sh
+++ create_superuser.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo "from django.contrib.auth.models import User" > createadmin.py
 echo "User.objects.create_superuser('admin', 'admin@email.io', 'password')" >> createadmin.py
 python manage.py shell < createadmin.py
